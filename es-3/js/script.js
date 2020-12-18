1bf610fe6993834bb05a2c1a70a9ce326e64af13
Simone-Ortu <simo.28.09o@gmail.com>
2020-12-18 12:05:50 +0100

    es 3

---
+++ es-3/js/script.js
@@ -0,0 +1,14 @@
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
+// chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+
+var invitati= ['Pippo', 'Paperino', 'Pluto'];
+
+var verifica = prompt('Inserire il proprio nome');
+
+if (invitati.some = verifica){
+    alert('sei un invitato, puoi pertecipare alla festa');
+}
+else{
+    alert('non sei in lista, non puoi partecipare')
+};
