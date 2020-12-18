a6f53a369e8b05e13a45569e930bf5459247d8b6
Simone-Ortu <simo.28.09o@gmail.com>
2020-12-18 10:57:37 +0100

    es 2

---
+++ es-2/js/script.js
@@ -0,0 +1,17 @@
+var numeri=[];
+var dispari=[];
+
+for(var i=0; i<6; i++){
+   numeri.push(prompt("Inserisci numero"));
+};
+console.log(numeri);
+for (var i=0; i<numeri.length ;i++){
+    if (numeri[i]%2 != 0){
+        dispari.push;
+        console.log('questi sono i dispari' , dispari)
+    }
+    else{
+        console.log(numeri);
+    }
+
+}
