d18777eb7348a961fa575e71bc30f68d7fbf2eaf
Simone-Ortu <simo.28.09o@gmail.com>
2020-12-18 10:13:20 +0100

    innit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var numero = [];
+var somma = 0;
+
+
+// for (var i = 0; i < 5; i++) {
+//     numero = parseInt(prompt('Inserisci un numero'));
+//     somma += numero;
+// };
+
+console.log(somma); 
+
+var i = 0;
+while(i < 5){
+    numero = parseInt(prompt('inserisci un numero'));
+    i++;
+    console.log(numero[i]);
+}
+somma += numero;
+    
+
