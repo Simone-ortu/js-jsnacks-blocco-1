0a159b40268955fcc298d0dc7bb27e5906dbe179
Simone-Ortu <simo.28.09o@gmail.com>
2020-12-18 11:01:56 +0100

    non capisco

--- es-2/js/script.js
+++ es-2/js/script.js
@@ -1,11 +1,12 @@
-var numeri=[];
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, 
+// se è dispari inseriscilo nell’array 
+
+
 var dispari=[];
 
-for(var i=0; i<6; i++){
-   numeri.push(prompt("Inserisci numero"));
-};
-console.log(numeri);
-for (var i=0; i<numeri.length ;i++){
+
+for (var i=0; i<6 ;i++){
+   var numeri = prompt('inserisci numero');
     if (numeri[i]%2 != 0){
         dispari.push;
         console.log('questi sono i dispari' , dispari)
